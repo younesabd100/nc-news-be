769c9f2928de4bf2ad7a57cb26b691107bbdc502
Younes <younes.abderral@gmail.com>
2025-03-11 18:21:06 +0000

    Completing task 8

--- model/model.enpoint.js
+++ model/model.enpoint.js
@@ -80,18 +80,24 @@ exports.updateArticleByArticleId = (article_id, inc_votes) => {
   if (!inc_votes) {
     return Promise.reject({ status: 400, msg: "Missing info" });
   }
-  if (typeof inc_votes !== "number") {
-    return Promise.reject({
-      status: 400,
-      msg: "Invalid data type entered",
-    });
-  }
   return db
     .query(
       `UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING * `,
       [inc_votes, article_id]
     )
     .then(({ rows }) => {
+      return rows[0];
+    });
+};
+exports.removeCommentByCommentId = (comment_id) => {
+  return db
+    .query(`DELETE FROM comments WHERE comment_id = $1 RETURNING * `, [
+      comment_id,
+    ])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Comment not found" });
+      }
       return rows[0];
     });
 };
